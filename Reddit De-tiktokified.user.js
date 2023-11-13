619579f949199067decd6a86bab3b846e56a3a75
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-11-13 10:35:30 -0600

    Formatting

--- Reddit De-tiktokified.user.js
+++ Reddit De-tiktokified.user.js
@@ -3,13 +3,15 @@
 // @version      0.0.1
 // @description  Redirects you to new.reddit.com, avoiding the new beta UI.
 // @license      MIT
-// @namespace    https://github.com/tadghh
-// @author       https://github.com/tadghh
-// @source	     https://github.com/tadghh/reddit-redirect
+// @namespace    https://github.com/tadghh/reddit-redirect/
+// @author       https://github.com/tadghh/
+// @source	     https://github.com/tadghh/reddit-redirect/
 // @match        *://www.reddit.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
+
 (function() {
     'use strict';
 
