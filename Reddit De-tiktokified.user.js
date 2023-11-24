fb8b7003ec935cf0930b3e57d42e556e3c4a843b
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-11-24 00:33:00 -0600

    Update Reddit De-tiktokified.user.js

--- Reddit De-tiktokified.user.js
+++ Reddit De-tiktokified.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Reddit De-tiktokified
-// @version      0.0.2
+// @version      0.0.4
 // @description  Redirects you to new.reddit.com, avoiding the new beta UI.
 // @license      MIT
 // @namespace    https://github.com/tadghh
@@ -8,32 +8,24 @@
 // @source	     https://github.com/tadghh/reddit-redirect
 // @match        *://www.reddit.com/*
 // @exclude      *://www.reddit.com/media*
-// @run-at       document-body
+// @run-at       document-start
 // ==/UserScript==
 (() => {
     'use strict';
 
-    if (location.pathname.indexOf('new.') === -1) {
+    const isRedditSessionCookieExists = document.cookie.includes('recent_srs');
+
+    if (!window.location.href.includes('new.reddit.com') && location.pathname.indexOf('new.') === -1 && isRedditSessionCookieExists) {
         var newURL = location.protocol;
 
         newURL += "//new.reddit.com";
         newURL += location.pathname;
         newURL += location.search;
         newURL += location.hash;
+
+        window.location.href = newURL;
+
         document.open();
         document.close();
-
-        document.body.style.transition = 'background-color 470ms ease-in-out';
-        setTimeout(() => {
-            document.body.style.backgroundColor = 'black';
-
-            document.body.addEventListener('transitionend', () => {
-                document.body.style.backgroundColor = 'white';
-
-                setTimeout(location = newURL, 221);
-            }, {
-                once: true
-            });
-        }, 250);
     }
 })();
